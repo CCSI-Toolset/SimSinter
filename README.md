9e135de0120f37158b1cad51a036184d769066d7
Val Hendrix <vchendrix@lbl.gov>
2017-07-07 16:01:33 -0700

    Issue #10 Build SimSinter
    + Added a getting started section to the README.md.
      This includes prerequisites and commands for cloning the repo
      and building/testing the source
    + Added two files from the MS merge modules.  We may want to remove
      these later but they seem to have a fairly open license. It should be
      confirmed however
    
      Master/CCSIUnitsInstaller/Microsoft_VC110_ATL_x86.msm
      Master/CCSIUnitsInstaller/Microsoft_VC110_CRT_x86.msm

--- README.md
+++ README.md
@@ -12,8 +12,31 @@
 * A practice of rebasing with the main repo should be used rather that merge commmits.
 
 ## Getting Started
+This software has been compiled and tested on Windows 7 professional
 
-TBD
+### Pre-requisites
+Your environment must have the following tools installed.
+The build has be tested with the following versions. Use other
+versions at your own risk.
+
++ Git Bash for windows
++ CMake 3.9
++ Microsoft Visual Studio 11.0
++ Microsoft .NET v4.0.30319
++ Wix Toolset v3.10
+
+### Build and Test
+After installing the tools above run the Git Bash program.
+Executing the commands below with compile the source and 
+run the tests.
+
+
+```
+git clone https://github.com/CCSI-Toolset/SimSinter.git
+cd SimSinter
+start make.bat
+
+```
 
 ## Authors
 
--- make.bat
+++ make.bat
@@ -1,5 +1,10 @@
 REM SimSinter make.bat
 
+
+cmake clean
+cmake --build Master\UC2\udunits2 --config Release
+
+
 REM TODO figure out path to nuget
 "C:\Program Files\NuGet\nuget.exe" restore Master 
 
@@ -8,4 +13,5 @@ REM TODO figure out path to MSBUILD
 "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release Master\SimSinter.sln 
 
 REM TODO figure out path to MSTest
-REM"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /resultsfile:SimSinter-Build.trx /test:SinterRegressionTests.SinterInputTests.ParseVariableTest /testcontainer:C:\Jenkins\workspace\SimSinter\Master\SinterRegressionTests\bin\Release\SinterRegressionTests.dll
+rm SimSinter-Build.trx
+"C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /resultsfile:SimSinter-Build.trx /test:SinterRegressionTests.SinterInputTests.ParseVariableTest /testcontainer:C:\Jenkins\workspace\SimSinter\Master\SinterRegressionTests\bin\Release\SinterRegressionTests.dll
