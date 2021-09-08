7b5c45e6def9cd677162b64ac5145c5c205215c2
Joshua Boverhof <jrboverhof@lbl.gov>
2021-09-08 01:33:19 -0700

    Update Version to 2.1.0.1

--- Master/AspenSinterGUI/Properties/AssemblyInfo.cs
+++ Master/AspenSinterGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CCSI")]
 [assembly: AssemblyProduct("SinterConfigGUI")]
-[assembly: AssemblyCopyright("Copyright © Lawrence Livermore Laboratory 2012")]
+[assembly: AssemblyCopyright("Copyright © Lawrence Livermore Laboratory 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("2.1.0.1")]
+[assembly: AssemblyFileVersion("2.1.0.1")]
